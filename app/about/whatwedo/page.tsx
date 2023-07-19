8c263902c45ac3d55800db8674fd1e0c20bef03d
Yerankim1230 <110910348+Yerankim1230@users.noreply.github.com>
2023-07-19 13:52:16 +0200

    About-What we do : Hero, Flowingboard

---
+++ app/about/whatwedo/page.tsx
@@ -0,0 +1,12 @@
+import WhatWeDo_FlowingBoard from '@/components/about/whatwedo/WhatWeDo_FlowingBoard'
+import WhatWeDo_Hero from '@/components/about/whatwedo/WhatWeDo_Hero'
+import React from 'react'
+
+export default function whatwedoPage() {
+  return (
+    <div>
+        <WhatWeDo_Hero/>
+        <WhatWeDo_FlowingBoard/>
+    </div>
+  )
+}
