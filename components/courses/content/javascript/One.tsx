14177fdbe5d7a100eb8d0dd78535e8fd3a0dbe87
Joel Buchholz <michelleundjoel@gmail.com>
2023-07-20 14:08:04 +0200

    fixesss

--- components/courses/content/javascript/One.tsx
+++ components/courses/content/javascript/One.tsx
@@ -1,10 +1,11 @@
 import React from "react"
-import Editor from "@monaco-editor/react"
+import Editor from "../Editor"
+
 
 const One = () => {
   return (
     <div className="exercise_editor">
-      <Editor height="100vh" width="50%" />
+      <Editor/>
     </div>
   )
 }
